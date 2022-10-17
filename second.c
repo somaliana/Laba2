07c304e9d1d3fb437e56568c491955951c101185
Your Name <somaliana12@yandex.ru>
2022-10-17 19:26:14 +0300

    21321

--- second.c
+++ second.c
@@ -2,23 +2,24 @@
 #include<string.h>
 
 int main() {
-    int x,y,sign;
+    double x,y; 
+    int sign;
     
     printf("1=+,2=-,3=*,4=/ \n");
-    scanf("%d %d",&x,&y);
+    scanf("%lf %lf",&x,&y);
     scanf("%d",&sign);
     switch(sign) {
         case 1:
-            printf("%d",x+y);
+            printf("%lf",x+y);
             break;
         case 2:
-            printf("%d",x-y);
+            printf("%lf",x-y);
             break;
         case 3:
-            printf("%d",x*y);
+            printf("%lf",x*y);
             break;
         case 4:
-            printf("%d",x/y);
+            printf("%lf",x/y);
             break;
         default:
             printf("yoreadog \n");
